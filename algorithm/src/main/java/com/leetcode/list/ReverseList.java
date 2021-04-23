82c6eb8f9da53f0cf7fd0e2c1a757f1d16c1f3b8
Anne <2515924898@qq.com>
2021-04-23 18:09:17 +0800

    链表反转-递归

--- algorithm/src/main/java/com/leetcode/list/ReverseList.java
+++ algorithm/src/main/java/com/leetcode/list/ReverseList.java
@@ -15,4 +15,28 @@ public class ReverseList {
         }
         return resultList.next;
     }
+
+    // 递归
+    public ListNode reverseList2(ListNode head){
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode newHead = reverseList2(head.next);// 5->4
+        head.next.next = head; // 3->4 变成 3->4->3
+        head.next = null; // 4->3
+        return newHead;// 5->4->3
+    }
+
+    public static void main(String[] args){
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        ReverseList reverseList = new ReverseList();
+        reverseList.reverseList2(node1);
+
+    }
 }
